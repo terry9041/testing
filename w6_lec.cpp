b9df2068e71fa44295314fb81014119b1d0fbb54
terry9041 <142947048+terry9041@users.noreply.github.com>
2023-10-20 17:04:32 +0000

    d

--- w6_lec.cpp
+++ w6_lec.cpp
@@ -1,6 +1,48 @@
+// Online C++ compiler to run C++ program online
 #include <iostream>
 using namespace std;
-int main()
+class RationalNumber
 {
-    cout << 'h' << endl;
+private:
+    int a,b;
+public:
+    int getNumerator() const;
+    
+    int getDenominator() const;
+   
+    void setNumerator (const int&numerator);
+    
+    void setDenominator (const int&denominator);
+    
+    void print() const;
+    
+};
+
+int RationalNumber::getNumerator() const
+    {
+        return a;
+    }
+int RationalNumber::getDenominator() const
+    {
+        return b;
+    }
+void RationalNumber::RationalNumber::setNumerator (const int&numerator)
+    {
+        a = numerator;
+    }
+void RationalNumber::setDenominator (const int&denominator)
+    {
+        b = denominator != 0? denominator: 1;
+    }
+void RationalNumber::print() const
+    { 
+        cout << a << '/' << b ;
+    }
+
+int main() 
+{
+    RationalNumber r1;
+    r1.setNumerator(3);
+    r1.setDenominator(5);
+    r1.print();
 }
