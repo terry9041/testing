bf21314a4ad58a372f69f2405c552a47f47d6588
terry9041 <142947048+terry9041@users.noreply.github.com>
2023-10-20 19:06:34 +0000

    add the new asm 3 file

---
+++ asm3.cpp
@@ -0,0 +1,29 @@
+// Example program
+#include <iostream>
+using namespace std;
+struct node
+{
+    double data;
+    node* link;
+};
+
+void selectionSort(node* &head)
+{
+    node * temp = head;
+    int length = 0;
+    while (temp != nullptr)
+    {
+        length++;
+        temp = temp -> link;
+    }
+    for (int i = 0; i < length; i++)
+    {
+        currentVal = 
+        minVal = head->data;
+        
+}
+
+int main()
+{
+  
+}
